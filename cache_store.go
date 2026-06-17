3c59c3ae86da710ce890c921eca9bb8b642bde5e
HNRow <hnrowei@gmail.com>
2026-06-17 13:35:39 -0700

    new

---
+++ cache_store.go
@@ -0,0 +1,62 @@
+package cache
+
+import (
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type CacheStoreInterface interface {
+	Get(key string) (string, error)
+	Unmarshal(key string, object interface{}) error
+	Set(key string, value interface{}) error
+	Fetch(key string, fc func() interface{}) (string, error)
+	Delete(key string) error
+}
+
+type RedisStoreInterface interface {
+	Keys(pattern string) ([]string, error)
+	GetByte(key string) ([]byte, error)
+	Get(key string) (string, error)
+	GetInt(key string) (int, error)
+	IncrBy(key string, value int64) (int64, error)
+	DecrBy(key string, value int64) (int64, error)
+	Unmarshal(key string, object interface{}) error
+	Set(key string, value interface{}) error
+	Fetch(key string, fc func() interface{}) (string, error)
+	Delete(key string) error
+	RPush(key string, value ...interface{}) error
+	LPop(key string) (string, error)
+	Del(key ...string) error
+	LRem(key string, value interface{}) error
+	LRange(key string) ([]string, error)
+	LPush(key string, value interface{}) error
+	LLen(key string) (int64, error)
+	LIndex(key string, index int64) (string, error)
+	HSet(key string, field string, value interface{}) error
+	HMSet(key string, fields map[string]interface{}) error
+	HGet(key string, field string) (string, error)
+	HLen(key string) (int64, error)
+	HDel(key string, field string) error
+	HExists(key string, field string) (bool, error)
+	HGetall(key string) (map[string]string, error)
+	SIsMember(key string, field string) (bool, error)
+	SMembers(key string) ([]string, error)
+	SRem(key string, value ...interface{}) error
+	Scan(cursor uint64, match string, count int64) ([]string, uint64, error)
+	MGet(keys []string) ([]interface{}, error)
+	SAdd(key string, value ...interface{}) error
+	SCard(key string) (int64, error)
+	SRandMember(key string) (string, error)
+	ZRangeWithScores(key string, start, stop int64) ([]redis.Z, error)
+	ZAdd(key string, members ...redis.Z) error
+	Expire(key string, seconds time.Duration) (bool, error)
+	Exists(key string) (int64, error)
+	SetNX(key string, value interface{}, lockDuration time.Duration) (bool, error)
+	ZRangeByScore(key string, min, max string) ([]string, error)
+	TTL(key string) (time.Duration, error)
+	HIncrBy(key string, field string, incr int64) (int64, error)
+	PSubscribe(channels ...string) *redis.PubSub
+	Eval(script string, keys []string, args ...interface{}) (int, error)
+	Do(cmd string, key string, seconds string) error
+}
